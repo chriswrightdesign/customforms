9239202e972be546f102a9fb7bd31958716debf9
chriswrightdesign <chriswright@Chriss-MacBook-Pro.local>
2015-03-11 17:47:08 +1100

    Test: touch

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,146 @@
+
+(function(){
+  "use strict";
+
+  var d = document,
+  $, eventType, prefixed, getDataAttribute;
+  var btnHorizonSubtract, btnHorizonAdd, 
+  spinnerHorizonInput, changeHorizonSpinner;
+
+  var btnVerticalAdd, btnVerticalSubtract,
+  spinnerVerticalInput, changeVerticalSpinner;
+
+  var btnPasswordSwitch, revealPassword, passwordInput;
+
+  var vendorPrefixes = ["", "webkit", "moz", "MS", "ms", "o"];
+
+  var mobileRegex = /mobile|tablet|ip(ad|hone|od)|android/i,
+  supportTouch = ("ontouchstart" in window), checkTouch, displayTouch, supportPointerEvents;
+  
+  var supportOnlyTouch = supportTouch && mobileRegex.test(navigator.userAgent);
+
+  eventType = supportTouch ? "touchstart" : "click";
+
+  
+  $ = function(selector){
+    return d.querySelector(selector);
+  };
+
+  //prefixed adapted from Hammer.js
+  prefixed = function(obj, property) {
+    var prefix, prop;
+    var camelProp = property[0].toUpperCase() + property.slice(1);
+
+    var i = 0;
+    while (i < vendorPrefixes.length) {
+        prefix = vendorPrefixes[i];
+        prop = (prefix) ? prefix + camelProp : property;
+
+        if (prop in obj) {
+            return prop;
+        }
+        i++;
+    }
+    return undefined;
+  };
+
+  supportPointerEvents = prefixed(window, "PointerEvent") !== undefined;
+
+  displayTouch = $(".js-displaytouch");
+
+  //password switcher
+  btnPasswordSwitch = $(".js-password-switch");
+  passwordInput = $(".js-password-input");
+
+  //vertical 
+  btnVerticalAdd = $(".js-btn-vertical-add");
+  btnVerticalSubtract = $(".js-btn-vertical-subtract");
+  spinnerVerticalInput = $(".js-spinner-input-vertical");
+
+  //horizontal
+  btnHorizonSubtract = $(".js-spinner-horizontal-subtract");
+  btnHorizonAdd = $(".js-spinner-horizontal-add");
+  spinnerHorizonInput = $(".js-spinner-input-horizontal");
+
+  checkTouch = function(){
+
+    displayTouch.textContent = "Support Touch: " + supportTouch + " , Support Pointer Events: " + supportPointerEvents + " , supports only Touch: " + supportOnlyTouch;
+
+  };
+  getDataAttribute = function(el, attr){
+
+    var hasGetAttr = (el.getAttribute && el.getAttribute(attr)) || null;
+    if(!hasGetAttr) {
+        var attrs = el.attributes;
+        for(var i = 0, len = attrs.length; i < len; i++){
+            if(attrs[i].nodeName === attr) {
+                hasGetAttr = attrs[i].nodeValue;
+            }
+        }
+    }
+    return hasGetAttr;
+
+  };
+
+  //password switcher
+  revealPassword = function(e){
+    e.preventDefault();
+
+    if(classie.has(btnPasswordSwitch, "is-active")){
+      passwordInput.setAttribute("type", "password");
+    } else {
+       passwordInput.setAttribute("type", "text");
+    }
+    classie.toggle(btnPasswordSwitch, "is-active");
+  };
+
+  //vertical
+  changeVerticalSpinner = function(e){
+    e.preventDefault();
+    var tmp = getDataAttribute(e.target, "data-type");
+    //var tmpValue = spinnerVerticalInput.value;
+    if(tmp === "add"){
+      spinnerVerticalInput.value++;
+    } else {
+      if(spinnerVerticalInput.value > 0) {
+        spinnerVerticalInput.value--;
+      }
+    }
+
+  };
+
+  //horizontal
+  changeHorizonSpinner = function(e){
+    e.preventDefault();
+    var tmp = getDataAttribute(e.target, "data-type");
+    //var tmpValue = spinnerHorizonInput.value;
+    if(tmp === "add"){
+      spinnerHorizonInput.value++;
+    } else {
+      if(spinnerHorizonInput.value > 0){
+        spinnerHorizonInput.value--; 
+      }
+    }
+  };
+
+  //password button
+  btnPasswordSwitch.addEventListener(eventType, revealPassword, false);
+ 
+  //vertical
+  btnVerticalAdd.addEventListener(eventType, changeVerticalSpinner, false);
+  btnVerticalSubtract.addEventListener(eventType, changeVerticalSpinner, false);
+
+  //horizontal
+  btnHorizonSubtract.addEventListener(eventType, changeHorizonSpinner, false);
+  btnHorizonAdd.addEventListener(eventType, changeHorizonSpinner, false);
+
+  checkTouch();
+})();
+
+/* Modernizr 2.7.1 (Custom Build) | MIT & BSD
+ * Build: http://modernizr.com/download/#-flexbox-csscolumns-history-inlinesvg-svg-svgclippaths-touch-webgl-cssclasses-addtest-prefixed-teststyles-testprop-testallprops-hasevent-prefixes-domprefixes-css_pointerevents-css_regions-svg_filters
+ */
+;window.Modernizr=function(a,b,c){function B(a){j.cssText=a}function C(a,b){return B(m.join(a+";")+(b||""))}function D(a,b){return typeof a===b}function E(a,b){return!!~(""+a).indexOf(b)}function F(a,b){for(var d in a){var e=a[d];if(!E(e,"-")&&j[e]!==c)return b=="pfx"?e:!0}return!1}function G(a,b,d){for(var e in a){var f=b[a[e]];if(f!==c)return d===!1?a[e]:D(f,"function")?f.bind(d||b):f}return!1}function H(a,b,c){var d=a.charAt(0).toUpperCase()+a.slice(1),e=(a+" "+o.join(d+" ")+d).split(" ");return D(b,"string")||D(b,"undefined")?F(e,b):(e=(a+" "+p.join(d+" ")+d).split(" "),G(e,b,c))}var d="2.7.1",e={},f=!0,g=b.documentElement,h="modernizr",i=b.createElement(h),j=i.style,k,l={}.toString,m=" -webkit- -moz- -o- -ms- ".split(" "),n="Webkit Moz O ms",o=n.split(" "),p=n.toLowerCase().split(" "),q={svg:"http://www.w3.org/2000/svg"},r={},s={},t={},u=[],v=u.slice,w,x=function(a,c,d,e){var f,i,j,k,l=b.createElement("div"),m=b.body,n=m||b.createElement("body");if(parseInt(d,10))while(d--)j=b.createElement("div"),j.id=e?e[d]:h+(d+1),l.appendChild(j);return f=["&#173;",'<style id="s',h,'">',a,"</style>"].join(""),l.id=h,(m?l:n).innerHTML+=f,n.appendChild(l),m||(n.style.background="",n.style.overflow="hidden",k=g.style.overflow,g.style.overflow="hidden",g.appendChild(n)),i=c(l,a),m?l.parentNode.removeChild(l):(n.parentNode.removeChild(n),g.style.overflow=k),!!i},y=function(){function d(d,e){e=e||b.createElement(a[d]||"div"),d="on"+d;var f=d in e;return f||(e.setAttribute||(e=b.createElement("div")),e.setAttribute&&e.removeAttribute&&(e.setAttribute(d,""),f=D(e[d],"function"),D(e[d],"undefined")||(e[d]=c),e.removeAttribute(d))),e=null,f}var a={select:"input",change:"input",submit:"form",reset:"form",error:"img",load:"img",abort:"img"};return d}(),z={}.hasOwnProperty,A;!D(z,"undefined")&&!D(z.call,"undefined")?A=function(a,b){return z.call(a,b)}:A=function(a,b){return b in a&&D(a.constructor.prototype[b],"undefined")},Function.prototype.bind||(Function.prototype.bind=function(b){var c=this;if(typeof c!="function")throw new TypeError;var d=v.call(arguments,1),e=function(){if(this instanceof e){var a=function(){};a.prototype=c.prototype;var f=new a,g=c.apply(f,d.concat(v.call(arguments)));return Object(g)===g?g:f}return c.apply(b,d.concat(v.call(arguments)))};return e}),r.flexbox=function(){return H("flexWrap")},r.webgl=function(){return!!a.WebGLRenderingContext},r.touch=function(){var c;return"ontouchstart"in a||a.DocumentTouch&&b instanceof DocumentTouch?c=!0:x(["@media (",m.join("touch-enabled),("),h,")","{#modernizr{top:9px;position:absolute}}"].join(""),function(a){c=a.offsetTop===9}),c},r.history=function(){return!!a.history&&!!history.pushState},r.csscolumns=function(){return H("columnCount")},r.svg=function(){return!!b.createElementNS&&!!b.createElementNS(q.svg,"svg").createSVGRect},r.inlinesvg=function(){var a=b.createElement("div");return a.innerHTML="<svg/>",(a.firstChild&&a.firstChild.namespaceURI)==q.svg},r.svgclippaths=function(){return!!b.createElementNS&&/SVGClipPath/.test(l.call(b.createElementNS(q.svg,"clipPath")))};for(var I in r)A(r,I)&&(w=I.toLowerCase(),e[w]=r[I](),u.push((e[w]?"":"no-")+w));return e.addTest=function(a,b){if(typeof a=="object")for(var d in a)A(a,d)&&e.addTest(d,a[d]);else{a=a.toLowerCase();if(e[a]!==c)return e;b=typeof b=="function"?b():b,typeof f!="undefined"&&f&&(g.className+=" "+(b?"":"no-")+a),e[a]=b}return e},B(""),i=k=null,e._version=d,e._prefixes=m,e._domPrefixes=p,e._cssomPrefixes=o,e.hasEvent=y,e.testProp=function(a){return F([a])},e.testAllProps=H,e.testStyles=x,e.prefixed=function(a,b,c){return b?H(a,b,c):H(a,"pfx")},g.className=g.className.replace(/(^|\s)no-js(\s|$)/,"$1$2")+(f?" js "+u.join(" "):""),e}(this,this.document),Modernizr.addTest("pointerevents",function(){var a=document.createElement("x"),b=document.documentElement,c=window.getComputedStyle,d;return"pointerEvents"in a.style?(a.style.pointerEvents="auto",a.style.pointerEvents="x",b.appendChild(a),d=c&&c(a,"").pointerEvents==="auto",b.removeChild(a),!!d):!1}),Modernizr.addTest("regions",function(){var a=Modernizr.prefixed("flowFrom"),b=Modernizr.prefixed("flowInto");if(!a||!b)return!1;var c=document.createElement("div"),d=document.createElement("div"),e=document.createElement("div"),f="modernizr_flow_for_regions_check";d.innerText="M",c.style.cssText="top: 150px; left: 150px; padding: 0px;",e.style.cssText="width: 50px; height: 50px; padding: 42px;",e.style[a]=f,c.appendChild(d),c.appendChild(e),document.documentElement.appendChild(c);var g,h,i=d.getBoundingClientRect();return d.style[b]=f,g=d.getBoundingClientRect(),h=g.left-i.left,document.documentElement.removeChild(c),d=e=c=undefined,h==42}),Modernizr.addTest("svgfilters",function(){var a=!1;try{a=typeof SVGFEColorMatrixElement!==undefined&&SVGFEColorMatrixElement.SVG_FECOLORMATRIX_TYPE_SATURATE==2}catch(b){}return a});
 !window.addEventListener && (function (WindowPrototype, DocumentPrototype, ElementPrototype, addEventListener, removeEventListener, dispatchEvent, registry) {
   WindowPrototype[addEventListener] = DocumentPrototype[addEventListener] = ElementPrototype[addEventListener] = function (type, listener) {
     var target = this;
@@ -101,121 +244,3 @@ if ( typeof define === 'function' && define.amd ) {
 }
 })( window );
 
-
-(function(){
-  "use strict";
-
-  var d = document,
-  $, eventType, getDataAttribute;
-  var btnHorizonSubtract, btnHorizonAdd, 
-  spinnerHorizonInput, changeHorizonSpinner;
-
-  var btnVerticalAdd, btnVerticalSubtract,
-  spinnerVerticalInput, changeVerticalSpinner;
-
-  var btnPasswordSwitch, revealPassword, passwordInput;
-
-  eventType = Modernizr.touch ? "touchstart" : "click";
-
-  $ = function(selector){
-    return d.querySelector(selector);
-  };
-
-  //password switcher
-  btnPasswordSwitch = $(".js-password-switch");
-  passwordInput = $(".js-password-input");
-
-  //vertical 
-  btnVerticalAdd = $(".js-btn-vertical-add");
-  btnVerticalSubtract = $(".js-btn-vertical-subtract");
-  spinnerVerticalInput = $(".js-spinner-input-vertical");
-
-  //horizontal
-  btnHorizonSubtract = $(".js-spinner-horizontal-subtract");
-  btnHorizonAdd = $(".js-spinner-horizontal-add");
-  spinnerHorizonInput = $(".js-spinner-input-horizontal");
-
-  getDataAttribute = function(el, attr){
-
-    var hasGetAttr = (el.getAttribute && el.getAttribute(attr)) || null;
-    if(!hasGetAttr) {
-        var attrs = ele.attributes;
-        for(var i = 0, len = attrs.length; i < len; i++){
-            if(attrs[i].nodeName === attr) {
-                hasGetAttr = attrs[i].nodeValue;
-            }
-        }
-    }
-    return hasGetAttr;
-
-  };
-
-  //password switcher
-  revealPassword = function(e){
-    e.preventDefault();
-
-
-    if(classie.has(btnPasswordSwitch, "is-active")){
-
-      passwordInput.setAttribute("type", "password");
-    } else {
-       passwordInput.setAttribute("type", "text");
-    }
-    classie.toggle(btnPasswordSwitch, "is-active");
-
-   
-
-  };
-
-  //vertical
-  changeVerticalSpinner = function(e){
-
-    e.preventDefault();
-    var tmp = getDataAttribute(e.target, "data-type");
-    var tmpValue = spinnerVerticalInput.value;
-    if(tmp == "add"){
-     
-      spinnerVerticalInput.value++;
-    } else {
-      
-      if(spinnerVerticalInput.value > 0) {
-        spinnerVerticalInput.value--;
-      }
-    }
-
-  };
-
-  //horizontal
-  changeHorizonSpinner = function(e){
-    e.preventDefault();
-    var tmp = getDataAttribute(e.target, "data-type");
-    var tmpValue = spinnerHorizonInput.value;
-    if(tmp == "add"){
-     
-      spinnerHorizonInput.value++;
-    } else {
-      if(spinnerHorizonInput.value > 0){
-        spinnerHorizonInput.value--; 
-      }
-
-    }
-   
-  };
-
-  //password button
-  btnPasswordSwitch.addEventListener(eventType, revealPassword, false);
- 
-  //vertical
-  btnVerticalAdd.addEventListener(eventType, changeVerticalSpinner, false);
-  btnVerticalSubtract.addEventListener(eventType, changeVerticalSpinner, false);
-
-  //horizontal
-  btnHorizonSubtract.addEventListener(eventType, changeHorizonSpinner, false);
-  btnHorizonAdd.addEventListener(eventType, changeHorizonSpinner, false);
-
-})();
-
-/* Modernizr 2.7.1 (Custom Build) | MIT & BSD
- * Build: http://modernizr.com/download/#-flexbox-csscolumns-history-inlinesvg-svg-svgclippaths-touch-webgl-cssclasses-addtest-prefixed-teststyles-testprop-testallprops-hasevent-prefixes-domprefixes-css_pointerevents-css_regions-svg_filters
- */
-;window.Modernizr=function(a,b,c){function B(a){j.cssText=a}function C(a,b){return B(m.join(a+";")+(b||""))}function D(a,b){return typeof a===b}function E(a,b){return!!~(""+a).indexOf(b)}function F(a,b){for(var d in a){var e=a[d];if(!E(e,"-")&&j[e]!==c)return b=="pfx"?e:!0}return!1}function G(a,b,d){for(var e in a){var f=b[a[e]];if(f!==c)return d===!1?a[e]:D(f,"function")?f.bind(d||b):f}return!1}function H(a,b,c){var d=a.charAt(0).toUpperCase()+a.slice(1),e=(a+" "+o.join(d+" ")+d).split(" ");return D(b,"string")||D(b,"undefined")?F(e,b):(e=(a+" "+p.join(d+" ")+d).split(" "),G(e,b,c))}var d="2.7.1",e={},f=!0,g=b.documentElement,h="modernizr",i=b.createElement(h),j=i.style,k,l={}.toString,m=" -webkit- -moz- -o- -ms- ".split(" "),n="Webkit Moz O ms",o=n.split(" "),p=n.toLowerCase().split(" "),q={svg:"http://www.w3.org/2000/svg"},r={},s={},t={},u=[],v=u.slice,w,x=function(a,c,d,e){var f,i,j,k,l=b.createElement("div"),m=b.body,n=m||b.createElement("body");if(parseInt(d,10))while(d--)j=b.createElement("div"),j.id=e?e[d]:h+(d+1),l.appendChild(j);return f=["&#173;",'<style id="s',h,'">',a,"</style>"].join(""),l.id=h,(m?l:n).innerHTML+=f,n.appendChild(l),m||(n.style.background="",n.style.overflow="hidden",k=g.style.overflow,g.style.overflow="hidden",g.appendChild(n)),i=c(l,a),m?l.parentNode.removeChild(l):(n.parentNode.removeChild(n),g.style.overflow=k),!!i},y=function(){function d(d,e){e=e||b.createElement(a[d]||"div"),d="on"+d;var f=d in e;return f||(e.setAttribute||(e=b.createElement("div")),e.setAttribute&&e.removeAttribute&&(e.setAttribute(d,""),f=D(e[d],"function"),D(e[d],"undefined")||(e[d]=c),e.removeAttribute(d))),e=null,f}var a={select:"input",change:"input",submit:"form",reset:"form",error:"img",load:"img",abort:"img"};return d}(),z={}.hasOwnProperty,A;!D(z,"undefined")&&!D(z.call,"undefined")?A=function(a,b){return z.call(a,b)}:A=function(a,b){return b in a&&D(a.constructor.prototype[b],"undefined")},Function.prototype.bind||(Function.prototype.bind=function(b){var c=this;if(typeof c!="function")throw new TypeError;var d=v.call(arguments,1),e=function(){if(this instanceof e){var a=function(){};a.prototype=c.prototype;var f=new a,g=c.apply(f,d.concat(v.call(arguments)));return Object(g)===g?g:f}return c.apply(b,d.concat(v.call(arguments)))};return e}),r.flexbox=function(){return H("flexWrap")},r.webgl=function(){return!!a.WebGLRenderingContext},r.touch=function(){var c;return"ontouchstart"in a||a.DocumentTouch&&b instanceof DocumentTouch?c=!0:x(["@media (",m.join("touch-enabled),("),h,")","{#modernizr{top:9px;position:absolute}}"].join(""),function(a){c=a.offsetTop===9}),c},r.history=function(){return!!a.history&&!!history.pushState},r.csscolumns=function(){return H("columnCount")},r.svg=function(){return!!b.createElementNS&&!!b.createElementNS(q.svg,"svg").createSVGRect},r.inlinesvg=function(){var a=b.createElement("div");return a.innerHTML="<svg/>",(a.firstChild&&a.firstChild.namespaceURI)==q.svg},r.svgclippaths=function(){return!!b.createElementNS&&/SVGClipPath/.test(l.call(b.createElementNS(q.svg,"clipPath")))};for(var I in r)A(r,I)&&(w=I.toLowerCase(),e[w]=r[I](),u.push((e[w]?"":"no-")+w));return e.addTest=function(a,b){if(typeof a=="object")for(var d in a)A(a,d)&&e.addTest(d,a[d]);else{a=a.toLowerCase();if(e[a]!==c)return e;b=typeof b=="function"?b():b,typeof f!="undefined"&&f&&(g.className+=" "+(b?"":"no-")+a),e[a]=b}return e},B(""),i=k=null,e._version=d,e._prefixes=m,e._domPrefixes=p,e._cssomPrefixes=o,e.hasEvent=y,e.testProp=function(a){return F([a])},e.testAllProps=H,e.testStyles=x,e.prefixed=function(a,b,c){return b?H(a,b,c):H(a,"pfx")},g.className=g.className.replace(/(^|\s)no-js(\s|$)/,"$1$2")+(f?" js "+u.join(" "):""),e}(this,this.document),Modernizr.addTest("pointerevents",function(){var a=document.createElement("x"),b=document.documentElement,c=window.getComputedStyle,d;return"pointerEvents"in a.style?(a.style.pointerEvents="auto",a.style.pointerEvents="x",b.appendChild(a),d=c&&c(a,"").pointerEvents==="auto",b.removeChild(a),!!d):!1}),Modernizr.addTest("regions",function(){var a=Modernizr.prefixed("flowFrom"),b=Modernizr.prefixed("flowInto");if(!a||!b)return!1;var c=document.createElement("div"),d=document.createElement("div"),e=document.createElement("div"),f="modernizr_flow_for_regions_check";d.innerText="M",c.style.cssText="top: 150px; left: 150px; padding: 0px;",e.style.cssText="width: 50px; height: 50px; padding: 42px;",e.style[a]=f,c.appendChild(d),c.appendChild(e),document.documentElement.appendChild(c);var g,h,i=d.getBoundingClientRect();return d.style[b]=f,g=d.getBoundingClientRect(),h=g.left-i.left,document.documentElement.removeChild(c),d=e=c=undefined,h==42}),Modernizr.addTest("svgfilters",function(){var a=!1;try{a=typeof SVGFEColorMatrixElement!==undefined&&SVGFEColorMatrixElement.SVG_FECOLORMATRIX_TYPE_SATURATE==2}catch(b){}return a});
--- src/js/main.js
+++ src/js/main.js
@@ -1,112 +1,9 @@
-!window.addEventListener && (function (WindowPrototype, DocumentPrototype, ElementPrototype, addEventListener, removeEventListener, dispatchEvent, registry) {
-  WindowPrototype[addEventListener] = DocumentPrototype[addEventListener] = ElementPrototype[addEventListener] = function (type, listener) {
-    var target = this;
-
-    registry.unshift([target, type, listener, function (event) {
-      event.currentTarget = target;
-      event.preventDefault = function () { event.returnValue = false };
-      event.stopPropagation = function () { event.cancelBubble = true };
-      event.target = event.srcElement || target;
-
-      listener.call(target, event);
-    }]);
-
-    this.attachEvent("on" + type, registry[0][3]);
-  };
-
-  WindowPrototype[removeEventListener] = DocumentPrototype[removeEventListener] = ElementPrototype[removeEventListener] = function (type, listener) {
-    for (var index = 0, register; register = registry[index]; ++index) {
-      if (register[0] == this && register[1] == type && register[2] == listener) {
-        return this.detachEvent("on" + type, registry.splice(index, 1)[0][3]);
-      }
-    }
-  };
-
-  WindowPrototype[dispatchEvent] = DocumentPrototype[dispatchEvent] = ElementPrototype[dispatchEvent] = function (eventObject) {
-    return this.fireEvent("on" + eventObject.type, eventObject);
-  };
-})(Window.prototype, HTMLDocument.prototype, Element.prototype, "addEventListener", "removeEventListener", "dispatchEvent", []);
-/*!
-* classie v1.0.1
-* class helper functions
-* from bonzo https://github.com/ded/bonzo
-* MIT license
-*
-* classie.has( elem, 'my-class' ) -> true/false
-* classie.add( elem, 'my-new-class' )
-* classie.remove( elem, 'my-unwanted-class' )
-* classie.toggle( elem, 'my-class' )
-*/
-/*jshint browser: true, strict: true, undef: true, unused: true */
-/*global define: false, module: false */
-( function( window ) {
-"use strict";
-// class helper functions from bonzo https://github.com/ded/bonzo
-function classReg( className ) {
-  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
-}
-// classList support for class management
-// altho to be fair, the api sucks because it won't accept multiple classes at once
-var hasClass, addClass, removeClass;
-if ( 'classList' in document.documentElement ) {
-  hasClass = function( elem, c ) {
-    return elem.classList.contains( c );
-  };
-  addClass = function( elem, c ) {
-    elem.classList.add( c );
-  };
-  removeClass = function( elem, c ) {
-    elem.classList.remove( c );
-  };
-}
-else {
-  hasClass = function( elem, c ) {
-    return classReg( c ).test( elem.className );
-  };
-  addClass = function( elem, c ) {
-    if ( !hasClass( elem, c ) ) {
-      elem.className = elem.className + ' ' + c;
-    }
-  };
-  removeClass = function( elem, c ) {
-    elem.className = elem.className.replace( classReg( c ), ' ' );
-  };
-}
-function toggleClass( elem, c ) {
-  var fn = hasClass( elem, c ) ? removeClass : addClass;
-  fn( elem, c );
-}
-var classie = {
-  // full names
-  hasClass: hasClass,
-  addClass: addClass,
-  removeClass: removeClass,
-  toggleClass: toggleClass,
-  // short names
-  has: hasClass,
-  add: addClass,
-  remove: removeClass,
-  toggle: toggleClass
-};
-// transport
-if ( typeof define === 'function' && define.amd ) {
-  // AMD
-  define( classie );
-} else if ( typeof exports === 'object' ) {
-  // CommonJS
-  module.exports = classie;
-} else {
-  // browser global
-  window.classie = classie;
-}
-})( window );
-
 
 (function(){
   "use strict";
 
   var d = document,
-  $, eventType, getDataAttribute;
+  $, eventType, prefixed, getDataAttribute;
   var btnHorizonSubtract, btnHorizonAdd, 
   spinnerHorizonInput, changeHorizonSpinner;
 
@@ -115,11 +12,41 @@ if ( typeof define === 'function' && define.amd ) {
 
   var btnPasswordSwitch, revealPassword, passwordInput;
 
-  eventType = Modernizr.touch ? "touchstart" : "click";
+  var vendorPrefixes = ["", "webkit", "moz", "MS", "ms", "o"];
 
+  var mobileRegex = /mobile|tablet|ip(ad|hone|od)|android/i,
+  supportTouch = ("ontouchstart" in window), checkTouch, displayTouch, supportPointerEvents;
+  
+  var supportOnlyTouch = supportTouch && mobileRegex.test(navigator.userAgent);
+
+  eventType = supportTouch ? "touchstart" : "click";
+
+  
   $ = function(selector){
     return d.querySelector(selector);
   };
+
+  //prefixed adapted from Hammer.js
+  prefixed = function(obj, property) {
+    var prefix, prop;
+    var camelProp = property[0].toUpperCase() + property.slice(1);
+
+    var i = 0;
+    while (i < vendorPrefixes.length) {
+        prefix = vendorPrefixes[i];
+        prop = (prefix) ? prefix + camelProp : property;
+
+        if (prop in obj) {
+            return prop;
+        }
+        i++;
+    }
+    return undefined;
+  };
+
+  supportPointerEvents = prefixed(window, "PointerEvent") !== undefined;
+
+  displayTouch = $(".js-displaytouch");
 
   //password switcher
   btnPasswordSwitch = $(".js-password-switch");
@@ -135,11 +62,16 @@ if ( typeof define === 'function' && define.amd ) {
   btnHorizonAdd = $(".js-spinner-horizontal-add");
   spinnerHorizonInput = $(".js-spinner-input-horizontal");
 
+  checkTouch = function(){
+
+    displayTouch.textContent = "Support Touch: " + supportTouch + " , Support Pointer Events: " + supportPointerEvents + " , supports only Touch: " + supportOnlyTouch;
+
+  };
   getDataAttribute = function(el, attr){
 
     var hasGetAttr = (el.getAttribute && el.getAttribute(attr)) || null;
     if(!hasGetAttr) {
-        var attrs = ele.attributes;
+        var attrs = el.attributes;
         for(var i = 0, len = attrs.length; i < len; i++){
             if(attrs[i].nodeName === attr) {
                 hasGetAttr = attrs[i].nodeValue;
@@ -154,30 +86,22 @@ if ( typeof define === 'function' && define.amd ) {
   revealPassword = function(e){
     e.preventDefault();
 
-
     if(classie.has(btnPasswordSwitch, "is-active")){
-
       passwordInput.setAttribute("type", "password");
     } else {
        passwordInput.setAttribute("type", "text");
     }
     classie.toggle(btnPasswordSwitch, "is-active");
-
-   
-
   };
 
   //vertical
   changeVerticalSpinner = function(e){
-
     e.preventDefault();
     var tmp = getDataAttribute(e.target, "data-type");
-    var tmpValue = spinnerVerticalInput.value;
-    if(tmp == "add"){
-     
+    //var tmpValue = spinnerVerticalInput.value;
+    if(tmp === "add"){
       spinnerVerticalInput.value++;
     } else {
-      
       if(spinnerVerticalInput.value > 0) {
         spinnerVerticalInput.value--;
       }
@@ -189,17 +113,14 @@ if ( typeof define === 'function' && define.amd ) {
   changeHorizonSpinner = function(e){
     e.preventDefault();
     var tmp = getDataAttribute(e.target, "data-type");
-    var tmpValue = spinnerHorizonInput.value;
-    if(tmp == "add"){
-     
+    //var tmpValue = spinnerHorizonInput.value;
+    if(tmp === "add"){
       spinnerHorizonInput.value++;
     } else {
       if(spinnerHorizonInput.value > 0){
         spinnerHorizonInput.value--; 
       }
-
     }
-   
   };
 
   //password button
@@ -213,4 +134,5 @@ if ( typeof define === 'function' && define.amd ) {
   btnHorizonSubtract.addEventListener(eventType, changeHorizonSpinner, false);
   btnHorizonAdd.addEventListener(eventType, changeHorizonSpinner, false);
 
+  checkTouch();
 })();
